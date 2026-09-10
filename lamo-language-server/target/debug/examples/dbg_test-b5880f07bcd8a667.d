6f1b6b1ffe8f123928b422179137ed07ada0fe5e
arthurlamonattopro <arthurlamonattopro@gmail.com>
2026-09-10 20:28:31 -0300

    Fixed

--- lamo-language-server/target/debug/examples/dbg_test-b5880f07bcd8a667.d
+++
@@ -1,5 +0,0 @@
-C:\Users\LamoDev\Downloads\lamo\lamo-language-server\target\debug\examples\dbg_test-b5880f07bcd8a667.d: examples\dbg_test.rs
-
-C:\Users\LamoDev\Downloads\lamo\lamo-language-server\target\debug\examples\libdbg_test-b5880f07bcd8a667.rmeta: examples\dbg_test.rs
-
-examples\dbg_test.rs:
